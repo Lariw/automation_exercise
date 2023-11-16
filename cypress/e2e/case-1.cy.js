2598b89a4438a9519478f77c140880f3cf9ce6ac
Kamil Hajduła <98982966+Lariw@users.noreply.github.com>
2023-11-16 20:07:32 +0100

    Added TC-12 - Add Products in Cart.

--- cypress/e2e/case-1.cy.js
+++ cypress/e2e/case-1.cy.js
@@ -329,4 +329,59 @@ describe("Case-1", () => {
       "You have been successfully subscribed!"
     );
   });
+
+  it("Add Products in Cart", () => {
+    cy.visit(mainData.baseURI + "/");
+    cy.url().should("eq", mainData.baseURI + "/");
+
+    cy.get("section").should("be.visible");
+    cy.get("header").should("be.visible");
+    cy.get("footer").should("be.visible");
+
+    cy.get(".nav.navbar-nav > li > a").contains(" Products").click();
+    cy.url().should("eq", mainData.baseURI + "/products");
+
+    let prices = [];
+    let names = [];
+
+    cy.get(".productinfo.text-center")
+      .eq(0)
+      .then((element) => {
+        const price = Cypress.$(element).find("h2").text();
+        const name = Cypress.$(element).find("p").text();
+
+        prices.push(price);
+        names.push(name);
+      });
+
+    cy.get(".productinfo.text-center > .add-to-cart").eq(0).click();
+
+    cy.get('[data-dismiss="modal"]').click();
+
+    cy.get(".productinfo.text-center")
+      .eq(1)
+      .then((element) => {
+        const price = Cypress.$(element).find("h2").text();
+        const name = Cypress.$(element).find("p").text();
+
+        prices.push(price);
+        names.push(name);
+      });
+
+    cy.get(".productinfo.text-center > .add-to-cart").eq(1).click();
+
+    cy.get("a > u").click();
+
+    cy.url().should("eq", mainData.baseURI + "/view_cart");
+
+    cy.then(() => {
+      cy.get(".cart_description > h4 > a").eq(0).should("have.text", names[0]);
+      cy.get(".cart_description > h4 > a").eq(1).should("have.text", names[1]);
+    });
+
+    cy.then(() => {
+      cy.get(".cart_price > p").eq(0).should("have.text", prices[0]);
+      cy.get(".cart_price > p").eq(1).should("have.text", prices[1]);
+    });
+  });
 });
