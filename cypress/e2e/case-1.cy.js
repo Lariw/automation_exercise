4bb6f62dd7b899773b9800f7a7421e417a2b55e9
Kamil Hajduła <98982966+Lariw@users.noreply.github.com>
2023-11-21 00:38:38 +0100

    Added TC-14 - Place Order: Register while Checkout.

--- cypress/e2e/case-1.cy.js
+++ cypress/e2e/case-1.cy.js
@@ -404,4 +404,197 @@ describe("Case-1", () => {
 
     cy.get(".cart_quantity > button").should("have.text", "4");
   });
+
+  it("Place Order: Register while Checkout", () => {
+    cy.visit(mainData.baseURI + "/");
+    cy.url().should("eq", mainData.baseURI + "/");
+
+    cy.get("section").should("be.visible");
+    cy.get("header").should("be.visible");
+    cy.get("footer").should("be.visible");
+
+    let prices = [];
+    let names = [];
+
+    cy.get(".productinfo.text-center")
+      .eq(0)
+      .then((element) => {
+        const price = Cypress.$(element).find("h2").text();
+        const name = Cypress.$(element).find("p").text();
+
+        prices.push(price);
+        names.push(name);
+      });
+
+    cy.get(".productinfo.text-center > .add-to-cart").eq(0).click();
+
+    cy.get('[data-dismiss="modal"]').click();
+
+    cy.get(".productinfo.text-center")
+      .eq(1)
+      .then((element) => {
+        const price = Cypress.$(element).find("h2").text();
+        const name = Cypress.$(element).find("p").text();
+
+        prices.push(price);
+        names.push(name);
+      });
+
+    cy.get(".productinfo.text-center > .add-to-cart").eq(1).click();
+
+    cy.get('[href="/view_cart"] > u').click();
+    cy.url().should("eq", mainData.baseURI + "/view_cart");
+
+    cy.then(() => {
+      cy.get(".cart_description > h4 > a").eq(0).should("have.text", names[0]);
+      cy.get(".cart_description > h4 > a").eq(1).should("have.text", names[1]);
+    });
+
+    cy.then(() => {
+      cy.get(".cart_price > p").eq(0).should("have.text", prices[0]);
+      cy.get(".cart_price > p").eq(1).should("have.text", prices[1]);
+    });
+
+    cy.get(".check_out").contains("Proceed To Checkout").click();
+    cy.get('[href="/login"] > u').contains("Register / Login").click();
+
+    cy.url().should("eq", mainData.baseURI + "/login");
+
+    cy.get(".signup-form > h2").should("have.text", "New User Signup!");
+
+    cy.get('[data-qa="signup-name"]').type(userData.username);
+    cy.get('[data-qa="signup-email"]').type(userData.email);
+    cy.get('[data-qa="signup-button"]').click();
+
+    cy.url().should("eq", mainData.baseURI + "/signup");
+
+    cy.get(".title.text-center > b").eq(0).should("be.visible");
+    cy.get(".title.text-center > b ")
+      .eq(0)
+      .should("have.text", "Enter Account Information");
+
+    if (userData.gender === "male") {
+      cy.get("#id_gender1").check();
+    } else if (userData.gender === "female") {
+      cy.get("#id_gender2").check();
+    } else {
+      throw new Error("Invalid gender value in userData.json");
+    }
+
+    cy.get('[data-qa="password"]').type(userData.passwd);
+
+    cy.then(() => {
+      const [day, month, year] = userData.birthDate.split(".");
+
+      cy.get('[data-qa="days"]').select(day);
+      cy.get('[data-qa="months"]').select(month);
+      cy.get('[data-qa="years"]').select(year);
+    });
+
+    cy.get("#newsletter").check();
+    cy.get("#optin").check();
+
+    cy.get('[data-qa="first_name"]').type(userData.firstName);
+    cy.get('[data-qa="last_name"]').type(userData.lastName);
+    cy.get('[data-qa="company"]').type(userData.company);
+    cy.get('[data-qa="address"]').type(userData.address);
+    cy.get('[data-qa="first_name"]').type(userData.firstName);
+    cy.get('[data-qa="country"]').select(userData.country);
+    cy.get('[data-qa="state"]').type(userData.state);
+    cy.get('[data-qa="city"]').type(userData.city);
+    cy.get('[data-qa="zipcode"]').type(userData.zipcode);
+    cy.get('[data-qa="mobile_number"]').type(userData.mobileNumber);
+    cy.get('[data-qa="create-account"]')
+      .should("have.text", "Create Account")
+      .click();
+
+    cy.url().should("eq", mainData.baseURI + "/account_created");
+
+    cy.get(".title.text-center > b").should("be.visible");
+    cy.get(".title.text-center > b").should("have.text", "Account Created!");
+    cy.get('[data-qa="continue-button"]')
+      .should("have.text", "Continue")
+      .click();
+
+    cy.get(".nav.navbar-nav > li > a")
+      .contains(` Logged in as ${userData.username}`)
+      .should("be.visible");
+
+    cy.get(".nav.navbar-nav > li > a").contains(" Cart").click();
+    cy.get(".check_out").contains("Proceed To Checkout").click();
+
+    cy.url().should("eq", mainData.baseURI + "/checkout");
+
+    cy.get("#address_delivery > .address_firstname.address_lastname").should(
+      "contain.text",
+      userData.firstName
+    );
+    cy.get("#address_delivery > .address_firstname.address_lastname").should(
+      "contain.text",
+      userData.lastName
+    );
+    cy.get("#address_delivery > .address_address1.address_address2")
+      .eq(0)
+      .should("have.text", userData.company);
+    cy.get("#address_delivery > .address_address1.address_address2")
+      .eq(1)
+      .should("contain.text", userData.address);
+    cy.get(
+      "#address_delivery > .address_city.address_state_name.address_postcode"
+    ).should("contain.text", userData.city);
+    cy.get(
+      "#address_delivery > .address_city.address_state_name.address_postcode"
+    ).should("contain.text", userData.state);
+    cy.get(
+      "#address_delivery > .address_city.address_state_name.address_postcode"
+    ).should("contain.text", userData.zipcode);
+    cy.get("#address_delivery > .address_country_name").should(
+      "have.text",
+      userData.country
+    );
+    cy.get("#address_delivery > .address_phone").should(
+      "have.text",
+      userData.mobileNumber
+    );
+
+    cy.then(() => {
+      cy.get(".cart_description > h4 > a").eq(0).should("have.text", names[0]);
+      cy.get(".cart_description > h4 > a").eq(1).should("have.text", names[1]);
+    });
+
+    cy.then(() => {
+      cy.get(".cart_price > p").eq(0).should("have.text", prices[0]);
+      cy.get(".cart_price > p").eq(1).should("have.text", prices[1]);
+    });
+
+    cy.get(".form-control").type("Lorem ipsum dolor sodoles.");
+
+    cy.get('[href="/payment"]').contains("Place Order").click();
+
+    cy.url().should("eq", mainData.baseURI + "/payment");
+
+    cy.get('[data-qa="name-on-card"]').type(userData.payment.cardName);
+
+    cy.get('[data-qa="card-number"]').type(userData.payment.cardNumber);
+
+    cy.get('[data-qa="cvc"]').type(userData.payment.cvc);
+
+    cy.get('[data-qa="expiry-month"]').type(userData.payment.expirationM);
+
+    cy.get('[data-qa="expiry-year"]').type(userData.payment.expirationY);
+
+    cy.get('[data-qa="pay-button"]').contains("Pay and Confirm Order").click();
+
+    cy.get(".nav.navbar-nav > li > a")
+      .contains(" Delete Account")
+      .should("be.visible")
+      .click();
+
+    cy.url().should("eq", mainData.baseURI + "/delete_account");
+    cy.get(".title.text-center > b").should("be.visible");
+    cy.get(".title.text-center > b").should("have.text", "Account Deleted!");
+    cy.get('[data-qa="continue-button"]')
+      .should("have.text", "Continue")
+      .click();
+  });
 });
