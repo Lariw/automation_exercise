ab297ad6db9c017c73c6fd699725bcf0f73590b0
Kamil Hajduła <98982966+Lariw@users.noreply.github.com>
2023-11-26 23:04:56 +0100

    Added TC-24 - Download Invoice after purchase order.

--- cypress/e2e/case-1.cy.js
+++ cypress/e2e/case-1.cy.js
@@ -1,4 +1,6 @@
 describe("Case-1", () => {
+  const path = require("path");
+
   let userData = null;
   let mainData = null;
   let skipAddingCookies = true;
@@ -839,6 +841,70 @@ describe("Case-1", () => {
     });
 
     cy.checkoutVerification(userData, mainData);
+
+    cy.get(".nav.navbar-nav > li > a")
+      .contains(" Delete Account")
+      .should("be.visible")
+      .click();
+
+    cy.url().should("eq", mainData.baseURI + "/delete_account");
+    cy.get(".title.text-center > b").should("be.visible");
+    cy.get(".title.text-center > b").should("have.text", "Account Deleted!");
+    cy.get('[data-qa="continue-button"]')
+      .should("have.text", "Continue")
+      .click();
+  });
+
+  it("Download Invoice after purchase order", () => {
+    cy.visit(mainData.baseURI + "/");
+
+    cy.pageLoadVerification();
+
+    cy.get(".nav.navbar-nav > li > a").contains(" Signup / Login").click();
+
+    cy.url().should("eq", mainData.baseURI + "/login");
+
+    cy.userRegistration(userData, mainData);
+
+    let prices = [];
+    let names = [];
+
+    cy.get(".productinfo.text-center")
+      .eq(0)
+      .then((element) => {
+        const price = Cypress.$(element).find("h2").text();
+        const name = Cypress.$(element).find("p").text();
+
+        prices.push(price);
+        names.push(name);
+      });
+
+    cy.get(".productinfo.text-center > a").contains("Add to cart").click();
+
+    cy.get("a > u").contains("View Cart").click();
+
+    cy.url().should("eq", mainData.baseURI + "/view_cart");
+
+    cy.then(() => {
+      cy.get(".cart_description > h4 > a").eq(0).should("have.text", names[0]);
+      cy.get(".cart_price > p").eq(0).should("have.text", prices[0]);
+    });
+
+    cy.checkoutVerification(userData, mainData);
+
+    cy.get('[name="message"]').type(
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut tincidunt augue ex, sed pretium lectus ornare a. Duis efficitur elit at massa dictum, in scelerisque urna sodales. Maecenas vitae imperdiet erat. Ut condimentum blandit mauris non bibendum."
+    );
+
+    cy.userPayments(userData, mainData);
+
+    cy.get("div > a").contains("Download Invoice").click();
+
+    cy.wait(2000);
+
+    cy.readFile("cypress\\Downloads\\invoice.txt");
+
+    cy.get(".pull-right > a").contains("Continue").click();
 
     cy.get(".nav.navbar-nav > li > a")
       .contains(" Delete Account")
